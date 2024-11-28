79359c3431ef5e92999fecfd736331f9d2a76725
corbin murray <corbin.murray63@yahoo.com>
2024-11-28 13:16:06 -0600

    Init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mantine/core";
+import classes from "./page.module.css";
+
+export default function HomePage() {
+	return (
+		<>
+			<section className={classes.section}>
+				<div>hello</div>
+				<Button variant="filled">Hello</Button>
+			</section>
+		</>
+	);
+}
+ 
