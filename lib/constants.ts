0544ea43471181e6620fbc21bdcd51d892516503
corbin murray <corbin.murray63@yahoo.com>
2024-12-13 23:44:04 -0600

    Remove old

--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const EMAIL_ADDRESS = "corbin.murray63@gmail.com";
-export const GITHUB_URL = "https://github.com/corbinmurray";
-export const LINKEDIN_URL = "www.linkedin.com/in/corbin-murray-16a485182";
