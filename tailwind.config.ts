fdc087ca44c57265b3f6ac8505b8336a3bd9ae44
corbin murray <corbin.murray63@yahoo.com>
2024-12-16 16:00:43 -0600

    Set fontFamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,10 @@ export default {
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
 			},
+			fontFamily: {
+				sans: "var(--font-sans)",
+				mono: "var(--font-mono)",
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
