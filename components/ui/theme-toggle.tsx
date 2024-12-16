bb87b727720661c5ddad585e38f686cd503089c1
corbin murray <corbin.murray63@yahoo.com>
2024-12-16 16:25:05 -0600

    Using switch with sun and moon svgs

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
@@ -16,8 +17,7 @@ const ThemeToggle = () => {
 	};
 
 	return (
-		<label className="grid cursor-pointer place-items-center">
-			<input type="checkbox" value="default" className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1" onClick={toggleTheme} />
+		<div className="flex items-center space-x-2">
 			<svg
 				className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
 				xmlns="http://www.w3.org/2000/svg"
@@ -32,6 +32,9 @@ const ThemeToggle = () => {
 				<circle cx="12" cy="12" r="5" />
 				<path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
 			</svg>
+
+			<Switch onClick={toggleTheme} />
+
 			<svg
 				className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
 				xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +48,7 @@ const ThemeToggle = () => {
 				strokeLinejoin="round">
 				<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
 			</svg>
-		</label>
+		</div>
 	);
 };
 
