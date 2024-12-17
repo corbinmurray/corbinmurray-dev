bcc252158aca34514ffe8332ef116d4f6f1b2bf0
corbin murray <corbin.murray63@yahoo.com>
2024-12-16 18:26:45 -0600

    Add static content

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const EMAIL_ADDRESS = "corbin.murray63@gmail.com";
+export const GITHUB_URL = "https://github.com/corbinmurray";
+export const LINKEDIN_URL = "www.linkedin.com/in/corbin-murray-16a485182";
+export const THEME_NAME = "sunfire"
+export const LIGHT_THEME = THEME_NAME + "-light";
+export const DARK_THEME = THEME_NAME + "-dark";
