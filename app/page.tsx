c715ab646f3a4203a33f028ca3461566b42fa355
corbin murray <corbin.murray63@yahoo.com>
2024-12-24 15:13:40 -0600

    Move exp to its own page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import RevealSection from "@/components/ui/reveal-section";
 import SectionHeader from "@/components/ui/section-header";
 import SocialLinks from "@/components/ui/social-links";
-import experiences from "@/lib/experiences.json";
 import projects from "@/lib/projects.json";
 import { Github, SquareArrowOutUpRight } from "lucide-react";
 
@@ -30,47 +28,6 @@ export default async function Home() {
 					<SocialLinks orientation="horizontal" className="md:gap-12" />
 				</div>
 			</RevealSection>
-
-
-
-			<RevealSection id="experience" className="scroll-mt-40">
-				<SectionHeader sequence={2} title="Experience" />
-			</RevealSection>
-
-			{experiences.map((experience, i) => {
-				return (
-					<RevealSection key={i}>
-						<article className="flex flex-col gap-y-3 mb-16 border-b">
-							<div className="flex justify-start items-baseline">
-								<h3 className="m-0">{experience.companyName}</h3>
-								<h3 className="italic text-sm ms-10">{experience.dateRange}</h3>
-							</div>
-
-							<div>
-								<span className="text-primary font-semibold text-md">{experience.positionTitle}</span>
-							</div>
-
-							{experience.descriptions.map((description, ii) => {
-								return (
-									<p key={ii} className="mb-0">
-										{description}
-									</p>
-								);
-							})}
-
-							<ul className="list-none p-0 m-0 flex flex-row flex-wrap">
-								{experience.skills.map((skill, ii) => {
-									return (
-										<li key={ii} className="text-nowrap">
-											<Badge variant="accent">{skill}</Badge>
-										</li>
-									);
-								})}
-							</ul>
-						</article>
-					</RevealSection>
-				);
-			})}
 
 			<RevealSection id="projects" className="mb-32 md:mb-64 scroll-mt-40 mt-32 md:mt-64">
 				<SectionHeader sequence={3} title="Projects" />
