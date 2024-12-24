a7d13d8ea6e8d40623aa1ce598bb07bb85a5778d
corbin murray <corbin.murray63@yahoo.com>
2024-12-24 15:16:05 -0600

    Move contact to its own page

---
+++ app/contact/page.tsx
@@ -0,0 +1,22 @@
+import RevealSection from "@/components/ui/reveal-section";
+import SectionHeader from "@/components/ui/section-header";
+import SocialLinks from "@/components/ui/social-links";
+
+export default async function Page() {
+	return (
+		<>
+			<RevealSection>
+				<SectionHeader sequence={4} title="Contact Me" />
+
+				<article>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam ab nulla nam corrupti fugiat inventore atque temporibus vero, ut unde ratione sequi
+						sit architecto dicta dolorem, praesentium recusandae cumque? Ex!
+					</p>
+				</article>
+
+				<SocialLinks orientation="horizontal" className="mt-16 md:gap-12" />
+			</RevealSection>
+		</>
+	);
+}
