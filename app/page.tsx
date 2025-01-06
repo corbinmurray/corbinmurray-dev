c4238850ec911c6add24d483408a293caae89864
corbin murray <corbin.murray63@yahoo.com>
2025-01-06 09:04:05 -0600

    Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import RevealSection from "@/components/reveal-section";
+import SectionHeader from "@/components/section-header";
 
 export default async function Home() {
 	return (
 		<RevealSection className="mt-12 md:mt-16">
-			<h1 className="text-[clamp(14px,5vw,16px)] md:text-[clamp(14px,5vw,18px)] font-mono">Hi, my name is</h1>
+			<SectionHeader title="Welcome to my digital garden" punctuation="exclamation mark" />
 
-			<h2 className="text-[clamp(40px,8vw,60px)] font-semibold border-none leading-tight text-primary py-3">Corbin Murray</h2>
 
-			<article className="prose-p:m-0">
+			<article>
 				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione sit, optio recusandae cupiditate minus odio distinctio unde quisquam adipisci
-					molestiae? Non quam atque dolore sunt! Modi odit accusantium minus?
+					Hi, 👋 my name is <span className="text-primary font-semibold text-lg">Corbin Murray</span>. I am a{" "}
+					<span className="font-bold">software engineer</span> who loves to solve complex problems.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium laudantium impedit distinctio eveniet neque. Architecto, recusandae consequatur
+					quidem ex exercitationem, harum, ab nobis rem aliquid quibusdam sint dolorem veniam sapiente?
 				</p>
 			</article>
 		</RevealSection>
