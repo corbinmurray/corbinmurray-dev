139c08edd4bedb06d473c52f20506d880ac3653f
corbin murray <corbin.murray63@yahoo.com>
2025-01-08 20:08:10 -0600

    Add mazesvg

--- app/projects/asterius/page.tsx
+++ app/projects/asterius/page.tsx
@@ -1,3 +1,9 @@
-export default async function Page() {
-	return <div>Asterius page</div>;
+import MazeSvg from "@/components/maze-svg";
+
+export default function Page() {
+	return (
+		<div className="max-w-screen-sm mx-auto mt-28 xl:col-span-full">
+			<MazeSvg className="w-full h-full" />
+		</div>
+	);
 }
