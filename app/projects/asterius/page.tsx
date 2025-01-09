d74dac665914579e63eedbfdf7d5ff54b3969156
corbin murray <corbin.murray63@yahoo.com>
2025-01-09 14:52:28 -0600

    Add page contents

--- app/projects/asterius/page.tsx
+++ app/projects/asterius/page.tsx
@@ -24,6 +24,45 @@ export default function Page() {
 				<div className="max-w-screen-sm mx-auto my-12 md:my-24">
 					<MazeSvg className="mx-auto" />
 				</div>
+
+				<article className="mt-12 md:mt-24">
+					<h2 className="max-w-fit">Coming Up!</h2>
+				</article>
+
+				<article>
+					<p>
+						The Asterius project has an exciting future, with plans to expand its capabilities and make it even more engaging for users. A key focus is on
+						adding more maze-solving algorithms, giving users the ability to explore and compare how different techniques approach the same problem. Improved
+						animations are also in the works, aiming to create smoother, more dynamic visualizations that make the underlying mechanics of the algorithms easier
+						to understand.
+					</p>
+				</article>
+
+				<article>
+					<p>
+						Looking ahead, there’s also a plan to introduce a built-in code editor, giving users the opportunity to experiment with algorithms directly in the
+						app. By tweaking the code and seeing the results in real-time, users can gain a deeper understanding of how their changes affect the algorithm’s
+						performance and behavior.
+					</p>
+				</article>
+
+				<article>
+					<p>Planned features include:</p>
+					<ul>
+						<li>
+							<strong>Additional maze-solving algorithms:</strong> Broaden the range of techniques to explore and compare.
+						</li>
+						<li>
+							<strong>Enhanced animations:</strong> Create smoother and more intuitive visualizations for better understanding.
+						</li>
+						<li>
+							<strong> Built-in code editor:</strong> Allow users to modify algorithms and visualize their changes instantly.
+						</li>
+					</ul>
+					<p>
+						These additions aim to make Asterius an invaluable tool for learning, experimentation, and discovery in the field of maze-solving and pathfinding.
+					</p>
+				</article>
 			</RevealSection>
 		</>
 	);
