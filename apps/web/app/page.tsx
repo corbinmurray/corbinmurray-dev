c4962d3cdcc766048a07c5ea2c38d721cf34d7b3
corbin murray <corbin.murray63@yahoo.com>
2025-01-15 11:46:11 -0600

    Remove

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@repo/ui/components/ui/button";
-
-export default function Home() {
-	return (
-		<div className="w-full bg-primary h-1/2">
-			<Button>Hello from main/web</Button>
-		</div>
-	);
-}
