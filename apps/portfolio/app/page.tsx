cfe998fab6a436d2647cfdd74ef75315e925d02d
corbin murray <corbin.murray63@yahoo.com>
2025-01-15 20:53:53 -0600

    Add remaining components

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@repo/ui/components/button";
+import SectionHeader from "@repo/ui/components/section-header";
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-svh">
+			<div className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-2xl font-bold">Hello World</h1>
+				<Button size="sm">Button</Button>
+				<SectionHeader title="test" />
+			</div>
+		</div>
+	);
 }
