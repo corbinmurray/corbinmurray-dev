efe8d512c6b4220bda209346cbd9d4d6af10b0ac
corbin murray <corbin.murray63@yahoo.com>
2025-01-15 21:16:56 -0600

    Add multi-zone redirect

--- apps/portfolio/next.config.mjs
+++ apps/portfolio/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/projects",
+        destination: `${process.env.PROJECTS_DOMAIN}/projects`
+      },
+      {
+        source: "/projects/:path",
+        destination: `${process.env.PROJECTS_DOMAIN}/projects/:path`
+      }
+    ]
+  }
 }
 
 export default nextConfig
