f4fe9520fc583d3aa23540296ebabc24c2f413da
corbin murray <corbin.murray63@yahoo.com>
2025-01-15 21:46:21 -0600

    Move projects list to projects home page

--- apps/projects/app/page.tsx
+++ apps/projects/app/page.tsx
@@ -1,11 +1,47 @@
+import projects from "@/lib/projects.json";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@repo/ui/components/card";
 import RevealSection from "@repo/ui/components/reveal-section";
 import SectionHeader from "@repo/ui/components/section-header";
+import { Github, SquareArrowOutUpRight } from "lucide-react";
 
 export default async function Home() {
 	return (
 		<>
 			<RevealSection>
 				<SectionHeader title="Projects I've worked on" />
+			</RevealSection>
+
+			<RevealSection id="projects" className="my-36 md:my-72 scroll-m-24">
+				<SectionHeader title="Projects I've worked on" />
+
+				<ul className="flex flex-col gap-8 xl:flex-row">
+					{projects.map((project, i) => {
+						return (
+							<RevealSection key={i}>
+								<li>
+									<Card className="shadow max-w-md">
+										<CardHeader>
+											<CardTitle className="text-secondary">{project.title}</CardTitle>
+										</CardHeader>
+										<CardContent>
+											<article>
+												<p>{project.description}</p>
+											</article>
+										</CardContent>
+										<CardFooter className="gap-6">
+											<a href={project.githubLink}>
+												<Github className="smooth-hover hover:text-secondary" />
+											</a>
+											<a href={project.liveSiteLink}>
+												<SquareArrowOutUpRight className="smooth-hover hover:text-secondary" />
+											</a>
+										</CardFooter>
+									</Card>
+								</li>
+							</RevealSection>
+						);
+					})}
+				</ul>
 			</RevealSection>
 		</>
 	);
