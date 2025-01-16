93a65e27c0200f093492f43ae165433a13340034
corbin murray <corbin.murray63@yahoo.com>
2025-01-16 15:33:26 -0600

    Fix links

--- apps/projects/app/page.tsx
+++ apps/projects/app/page.tsx
@@ -1,9 +1,9 @@
 import projects from "@/lib/projects.json";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@repo/ui/components/card";
+import Link from "@repo/ui/components/link";
 import RevealSection from "@repo/ui/components/reveal-section";
 import SectionHeader from "@repo/ui/components/section-header";
-import { Github, SquareArrowOutUpRight } from "lucide-react";
-import NextLink from "next/link";
+import { Github } from "lucide-react";
 
 export default async function Home() {
 	return (
@@ -12,32 +12,35 @@ export default async function Home() {
 				<SectionHeader title="Projects I've worked on" />
 
 				<ul className="flex flex-col gap-8 xl:flex-row">
-					{projects.map((project, i) => {
-						return (
-							<RevealSection key={i}>
-								<li>
-									<Card className="shadow max-w-md">
-										<CardHeader>
-											<CardTitle className="text-secondary">{project.title}</CardTitle>
-										</CardHeader>
-										<CardContent>
-											<article>
-												<p>{project.description}</p>
-											</article>
-										</CardContent>
-										<CardFooter className="gap-6">
-											<a href={project.githubLink}>
-												<Github className="smooth-hover hover:text-secondary" />
-											</a>
-											<NextLink href={project.liveSiteLink}>
-												<SquareArrowOutUpRight className="smooth-hover hover:text-secondary" />
-											</NextLink>
-										</CardFooter>
-									</Card>
-								</li>
-							</RevealSection>
-						);
-					})}
+					{projects
+						.sort((a, b) => (a.title < b.title ? -1 : 1))
+						.map((project, i) => {
+							return (
+								<RevealSection key={i}>
+									<li>
+										<Card className="shadow max-w-md">
+											<CardHeader>
+												<CardTitle className="text-secondary">
+													<Link variant="internal" href={project.liveSiteLink}>
+														{project.title}
+													</Link>
+												</CardTitle>
+											</CardHeader>
+											<CardContent>
+												<article>
+													<p>{project.description}</p>
+												</article>
+											</CardContent>
+											<CardFooter className="gap-6">
+												<a href={project.githubLink}>
+													<Github className="smooth-hover hover:text-secondary" />
+												</a>
+											</CardFooter>
+										</Card>
+									</li>
+								</RevealSection>
+							);
+						})}
 				</ul>
 			</RevealSection>
 		</>
