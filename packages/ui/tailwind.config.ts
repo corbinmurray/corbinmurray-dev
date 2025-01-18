e5573cf8096df5b3b10496d1ca4827351260fc4e
corbin murray <corbin.murray63@yahoo.com>
2025-01-18 12:59:59 -0600

    Fix astro, working. Now, add components

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -14,8 +14,8 @@ const config = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["var(--font-sans)", ...fontFamily.sans],
-				mono: ["var(--font-mono)", ...fontFamily.mono],
+				sans: ["Inter Variable", ...fontFamily.sans],
+				mono: ["JetBrains Mono Variable", ...fontFamily.mono],
 			},
 			colors: {
 				border: "hsl(var(--border))",
