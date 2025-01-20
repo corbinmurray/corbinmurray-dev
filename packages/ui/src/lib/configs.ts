8503580fde147ee48d0b17aa2475f64abce98bd3
corbin murray <corbin.murray63@yahoo.com>
2025-01-20 15:39:19 -0600

    Fix nav add logo

---
+++ packages/ui/src/lib/configs.ts
@@ -0,0 +1,25 @@
+export const LINKS = {
+	internal: [
+		{
+			name: "About Me",
+			href: "/#about",
+		},
+		{
+			name: "Experience",
+			href: "/#experience",
+		},
+		{
+			name: "Projects",
+			href: "/projects",
+		},
+		{
+			name: "Contact",
+			href: "/#contact",
+		},
+	],
+	external: {
+		github: "https://github.com/corbinmurray",
+		email: "corbin.murray63@gmail.com",
+		linkedin: "www.linkedin.com/in/corbin-murray-16a485182",
+	},
+};
