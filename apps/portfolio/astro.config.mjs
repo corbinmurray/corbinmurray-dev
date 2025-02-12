92c7df99751e8036eea4210f84ef85cfb6e6d1af
corbin murray <corbin.murray63@yahoo.com>
2025-02-11 19:49:48 -0600

    package upgrade

--- apps/portfolio/astro.config.mjs
+++ apps/portfolio/astro.config.mjs
@@ -2,17 +2,11 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [react()],
 	vite: {
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-				"@repo/ui": path.resolve("../", "../", "packages", "ui"),
-			},
-		},
+		plugins: [tailwind()],
 	},
 });
