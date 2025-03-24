b9e989122d1be54194de1228e2c95c27759c547f
corbin murray <corbin.murray63@yahoo.com>
2025-03-24 10:33:01 -0500

    Remove unused var

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,32 +66,12 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	// Inline script to prevent hydration mismatch
-	const setThemeScript = `
-	  (function() {
-		const theme = localStorage.getItem('theme');
-		if (theme) {
-		  document.documentElement.className = theme;
-		  document.documentElement.style.colorScheme = theme;
-		} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		  document.documentElement.className = 'dark';
-		  document.documentElement.style.colorScheme = 'dark';
-		} else {
-		  document.documentElement.className = 'light';
-		  document.documentElement.style.colorScheme = 'light';
-		}
-	  })();
-	`;
-
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<link rel="icon" href="/icon.svg" type="image/svg" sizes="any" />
 				<link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" sizes="any" />
-
-				{/* Inject the theme initializer script */}
-				{/* <script dangerouslySetInnerHTML={{ __html: setThemeScript }} /> */}
 
 				{/* Hello inspector 👋, no worries this is a completely private analytics alternative that allows me to see a simple page count for this website :) */}
 				<script defer src="https://umami.corbinmurray.dev/script.js" data-website-id="bbf88294-76ec-4a43-958c-eea82893445e"></script>
